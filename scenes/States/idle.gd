8c1a04b267b61d50ecaa486a1252c1b075a482db
Kennro03 <kennro03@gmail.com>
2025-07-03 13:49:34 +0200

    State Machine base
    Added the state machine model, as provided with the gdquest "Make a Finite State Machine in Godot 4" tutorial

---
+++ scenes/States/idle.gd
@@ -0,0 +1,16 @@
+extends PlayerState
+
+func enter(previous_state_path: String, _data := {}) -> void:
+	player.velocity.x = 0.0
+	##player.animation_player.play("idle")
+
+func physics_update(_delta: float) -> void:
+	player.velocity.y += player.gravity * _delta
+	player.move_and_slide()
+
+	if not player.is_on_floor():
+		finished.emit(FALLING)
+	elif Input.is_action_just_pressed("Up"):
+		finished.emit(JUMPING)
+	elif Input.is_action_pressed("Left") or Input.is_action_pressed("Right"):
+		finished.emit(RUNNING)
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,31 +1,5 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
 
-@export var speed = 400
-@export var dash_cooldown = 2
-
-
-func get_input():
-	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = input_direction * speed 
-
-func get_sprint():
-	if Input.is_action_pressed("Sprint"):
-		speed = 750
-	else : 
-		speed = 400
-
-func get_dash():
-	if Input.is_action_pressed("Dash") :
-		var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
-		velocity = input_direction * 1000
-	
-
-func _physics_process(_delta):
-	get_sprint()
-	get_input()
-	get_dash()
-	move_and_slide()
-
-
-func _on_dash_timer_timeout() -> void:
-	pass # Replace with function body.
+@export var speed := 500.0
+@export var gravity := 4000.0
+@export var jump_impulse := 1800.0
